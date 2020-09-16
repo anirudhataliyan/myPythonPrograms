0c8f62f70bb5533568650708c3df3810222624a0
anirudhataliyan <38790786+anirudhataliyan@users.noreply.github.com>
2020-09-16 11:24:01 +0530

    Delete a.py

--- a.py
+++
@@ -1,9 +0,0 @@
-a = int(input("Enter first number : "))
-b = int(input("Enter secound number : "))
-c = int(input("Enter third number : "))
-d = max(a,b,c)
-e = min(a,d,c)
-
-print("Print Max Value Is : " , d)
-
-print("Print min Value Is : " , e)
